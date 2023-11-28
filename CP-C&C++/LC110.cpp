8a4d24fb864edd334c776b751ebe24a0307043da
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-11-28 23:37:38 +0530

    Balance Binary Tree - Optimized solution added

--- CP-C&C++/LC110.cpp
+++ CP-C&C++/LC110.cpp
@@ -44,3 +44,47 @@
 //         }
 //     }
 // };
+
+
+
+
+// OPTMIZED SOLUTION
+
+// class Solution {
+// public:
+
+//     pair<bool,int> isBal(TreeNode* root)
+//     {
+//         if(root==NULL)
+//             {
+//                 pair<bool,int> p = make_pair(true,0);
+//                 return p;
+//             }
+            
+//             pair<bool,int> left=isBal(root->left);
+//             pair<bool,int> right=isBal(root->right);
+            
+//             bool leftAns=left.first;
+//             bool rightAns=right.first;
+            
+//             bool diff=abs(left.second-right.second)<=1;
+            
+//             pair<bool,int>ans;
+//             ans.second=max(left.second,right.second)+1;
+            
+//             if(diff && leftAns && rightAns)
+//             {
+//                 ans.first=true;
+//             }
+//             else
+//             {
+//                 ans.first=false;
+//             }
+//             return ans;
+//     }
+
+//     bool isBalanced(TreeNode* root)
+//     {  
+//         return isBal(root).first;
+//     }
+// };
